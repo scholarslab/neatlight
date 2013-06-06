c4bceadf69c7eef2fc3e3e6a7b86bf33fefd465c
David McClure <david.mcclure@virginia.edu>
2013-06-06 09:02:24 -0400

    Adds shell Gruntfile.js.

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+
+/* vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2 cc=76; */
+
+/**
+ * @package     omeka
+ * @subpackage  neatline-NeatLight
+ * @copyright   2012 Rector and Board of Visitors, University of Virginia
+ * @license     http://www.apache.org/licenses/LICENSE-2.0.html
+ */
+
+module.exports = function(grunt) {
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+
+  grunt.initConfig({
+    // TODO
+  });
+
+};
